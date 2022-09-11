cfde4683a43665a4c56525fdd57939b83daf424f
norio <norio@nmm.local>
2022-09-11 17:40:15 +0000

    add swagger to main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+const PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Generic Simple Samples')
+    .setDescription('Simple API samples description')
+    .setVersion('1.0')
+    .addTag('sample')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT);
+  console.log(`Listen port: ${PORT}`);
 }
 bootstrap();
