9b70ad108000d4d66724ac7dd201b3b14b98d716
norio <norio@kitamura.us>
2023-09-27 15:27:34 -0700

    add swagger-static

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'swagger-static'),
+      // serveRoot: process.env.NODE_ENV === 'development' ? '/' : '/swagger',
+      serveRoot: '/api',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
